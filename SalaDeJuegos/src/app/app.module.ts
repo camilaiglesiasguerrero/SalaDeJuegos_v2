80c09ff272884c4b85748131cfc4412b1e27084f
camilaiglesiasguerrero <camila.ig08@gmail.com>
2017-09-05 21:00:48 -0300

    Inicio + ruteo

--- SalaDeJuegos/src/app/app.module.ts
+++ SalaDeJuegos/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Router} from '@angular/router';
+import { AdivinaElNumeroComponent } from './componentes/adivina-el-numero/adivina-el-numero.component';
+
+let miRuteo = [{path:'adivina',component:AdivinaElNumeroComponent}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdivinaElNumeroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(miRuteo),
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
